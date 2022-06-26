5c2d7202663d7d106ffea95f106fe2b8f97c49ce
shabal1n <artur.shab@yandex.ru>
2022-06-26 23:55:56 +0600

    3HW post-discovery-service

---
+++ src/test/java/kz/dar/academy/postdiscoveryservice/PostDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dar.academy.postdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
